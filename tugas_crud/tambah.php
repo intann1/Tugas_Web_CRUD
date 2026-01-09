ce31d411e0b4fd6bef023aa2231d7434ad3afa9a
intann1 <150524113+intann1@users.noreply.github.com>
2026-01-09 18:02:10 +0700

    Create tambah.php

---
+++ tugas_crud/tambah.php
@@ -0,0 +1,8 @@
+<h2>Tambah Mata Kuliah</h2>
+
+<form method="post" action="simpan.php">
+Kode MK : <input type="text" name="kode"><br>
+Nama MK : <input type="text" name="nama"><br>
+SKS : <input type="number" name="sks"><br><br>
+<input type="submit" value="Simpan">
+</form>

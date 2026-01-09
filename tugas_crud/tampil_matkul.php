5b1affc5aa944a2281edc99eb76677f1a0630342
intann1 <150524113+intann1@users.noreply.github.com>
2026-01-09 18:01:41 +0700

    Update tampil_matkul.php

--- tugas_crud/tampil_matkul.php
+++ tugas_crud/tampil_matkul.php
@@ -1,13 +1,35 @@
 <?php
-$cnn = mysqli_connect("localhost","root","","db_akademik");
-
-$data = mysqli_query($cnn,"SELECT * FROM matakuliah");
-
-echo "<h3>Daftar Mata Kuliah</h3>";
-
-while($d = mysqli_fetch_array($data)){
-    echo "Kode MK : $d[kode_mk]<br>";
-    echo "Nama MK : $d[nama_mk]<br>";
-    echo "SKS : $d[sks]<br><hr>";
-}
+include "koneksi_db.php";
+$data = mysqli_query($koneksi, "SELECT * FROM matakuliah");
 ?>
+
+<h2>Daftar Mata Kuliah</h2>
+<a href="tambah.php">Tambah Data</a>
+<br><br>
+
+<table border="1">
+<tr>
+    <th>No</th>
+    <th>Kode MK</th>
+    <th>Nama MK</th>
+    <th>SKS</th>
+    <th>Aksi</th>
+</tr>
+
+<?php
+$no = 1;
+while ($d = mysqli_fetch_array($data)) {
+?>
+<tr>
+    <td><?php echo $no++; ?></td>
+    <td><?php echo $d['kode_mk']; ?></td>
+    <td><?php echo $d['nama_mk']; ?></td>
+    <td><?php echo $d['sks']; ?></td>
+    <td>
+        <a href="edit.php?id=<?php echo $d['id']; ?>">Edit</a>
+        <a href="hapus.php?id=<?php echo $d['id']; ?>">Hapus</a>
+    </td>
+</tr>
+<?php } ?>
+</table>
+
